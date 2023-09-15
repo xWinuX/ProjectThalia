065cb389c8702ba800b974ba253329f8497648b8
WinuX <edwin.baumann23@gmail.com>
2023-09-15 18:15:01 +0200

    added swap chain support details struct

--- include/ProjectThalia/VulkanContext.hpp
+++ include/ProjectThalia/VulkanContext.hpp
@@ -15,10 +15,16 @@ namespace ProjectThalia
 			void Destroy();
 
 		private:
+			struct SwapChainSupportDetails
+			{
+					VkSurfaceCapabilitiesKHR        capabilities;
+					std::vector<VkSurfaceFormatKHR> formats;
+					std::vector<VkPresentModeKHR>   presentModes;
+			};
+
 			VulkanDevice _vulkanDevice;
 			VkInstance   _instance = VK_NULL_HANDLE;
 			VkSurfaceKHR _surface  = VK_NULL_HANDLE;
-
 
 			void CreateInstance(SDL_Window* sdlWindow, const std::vector<const char*>& validationLayers);
 			void CreateSurface(SDL_Window* sdlWindow);
