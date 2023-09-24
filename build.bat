763de69a98115e320685b3afe5027ce19cb607c2
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-09-24 20:01:09 +0200

    improved build bat

--- build.bat
+++ build.bat
@@ -7,6 +7,16 @@ if "%1"=="" (
 set CONFIG=%1
 
 cmake -G "Visual Studio 17 2022" -S . -B build -DCMAKE_BUILD_TYPE=%CONFIG%
+if %ERRORLEVEL% neq 0 (
+  echo "CMake configuration failed. Exiting."
+  exit /b %ERRORLEVEL%
+)
+
 cmake --build build --target ProjectThalia --config %CONFIG%
-cd build\bin\%CONFIG%\
+if %ERRORLEVEL% neq 0 (
+  echo "Build failed. Exiting."
+  exit /b %ERRORLEVEL%
+)
+
+cd build\bin\%CONFIG%
 ProjectThalia.exe
