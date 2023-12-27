b0b5ccc95f90d9d5de50440b858467f842acb1a0
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-12-27 07:38:00 +0100

    added type to performance display

--- include/SplitEngine/Debug/Performance.hpp
+++ include/SplitEngine/Debug/Performance.hpp
@@ -12,6 +12,6 @@
 	auto end_time   = std::chrono::high_resolution_clock::now();                                    \
 	auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time); \
 	auto durationNs = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);  \
-	LOG("[Perf] {0}: {1}", text, durationMs.count());                                               \
-	LOG("[Perf] {0}: {1}", text, durationNs.count());                                               \
+	LOG("[Perf] {0}: {1} milliseconds", text, durationMs.count());                                  \
+	LOG("[Perf] {0}: {1} nanoseconds", text, durationNs.count());                                   \
 	}
