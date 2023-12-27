58a631a3e905e0c1a5474bd266c9286f4f5d84b8
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-12-27 06:58:32 +0100

    added benchmark macro

---
+++ include/SplitEngine/Debug/Performance.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Log.hpp"
+
+#include <chrono>
+
+#define BENCHMARK_BEGIN \
+	{                   \
+		auto start_time = std::chrono::high_resolution_clock::now();
+
+#define BENCHMARK_END(text)                                                                         \
+	auto end_time   = std::chrono::high_resolution_clock::now();                                    \
+	auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time); \
+	auto durationNs = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);  \
+	LOG("[Perf] {0}: {1}", text, durationMs.count());                                               \
+	LOG("[Perf] {0}: {1}", text, durationNs.count());                                               \
+	}
