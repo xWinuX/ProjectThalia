10b2ad55fcf87f67db976f8ca42dc8e26560e9a2
WinuX <37900754+xWinuX@users.noreply.github.com>
2023-12-31 07:23:50 +0100

    added output path to vulkan cmake

--- cmake/Vulkan.cmake
+++ cmake/Vulkan.cmake
@@ -4,7 +4,7 @@ find_package(Vulkan COMPONENTS glslc)
 find_program(glslc_executable NAMES glslc HINTS Vulkan::glslc)
 
 function(compile_shader target)
-    cmake_parse_arguments(PARSE_ARGV 1 arg "" "ENV;FORMAT" "SOURCES")
+    cmake_parse_arguments(PARSE_ARGV 1 arg "" "ENV;FORMAT;OUTPUT" "SOURCES")
 
     # Determine the build configuration folder (debug, Release, etc.)
     set(config_folder "$<CONFIG>")
@@ -16,18 +16,18 @@ function(compile_shader target)
         string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}" "" source ${source})
 
         add_custom_command(
-                OUTPUT ${CMAKE_BINARY_DIR}/bin/${config_folder}/${source}.${arg_FORMAT}  # Change output path
+                OUTPUT ${arg_OUTPUT}/${source}.${arg_FORMAT} # Change output path
                 DEPENDS ${source}
-                DEPFILE ${CMAKE_BINARY_DIR}/bin/${config_folder}/${source}.d
+                DEPFILE ${arg_OUTPUT}/${source}.d
                 COMMAND
                 glslangValidator
                 -e main
                 -gVS
                 -V
-                -o ${CMAKE_BINARY_DIR}/bin/${config_folder}/${source}.${arg_FORMAT}  # Change output path
+                -o ${arg_OUTPUT}/${source}.${arg_FORMAT}  # Change output path
                 ${CMAKE_CURRENT_SOURCE_DIR}/${source}
         )
 
-        target_sources(${target} PRIVATE ${CMAKE_BINARY_DIR}/bin/${config_folder}/${source}.${arg_FORMAT})  # Change source path
+        target_sources(${target} PRIVATE ${arg_OUTPUT}/${source}.${arg_FORMAT})  # Change source path
     endforeach ()
 endfunction()
