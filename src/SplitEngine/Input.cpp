da7bae01ad7d7e6660eec8a773bb9c7c1168e398
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-01-12 00:05:10 +0100

    updated input system

--- src/SplitEngine/Input.cpp
+++ src/SplitEngine/Input.cpp
@@ -2,8 +2,15 @@
 
 namespace SplitEngine
 {
-	std::unordered_map<int, bool>                Input::_keyDownStates    = std::unordered_map<int, bool>();
-	std::unordered_map<int, Input::PressedState> Input::_keyPressedStates = std::unordered_map<int, Input::PressedState>();
+	std::unordered_map<int, bool>                Input::_keyDownStates {};
+	std::unordered_map<int, Input::PressedState> Input::_keyPressedStates {};
+
+	std::unordered_map<int, Input::ButtonAction> Input::_buttonActions {};
+	std::unordered_map<int, Input::AxisAction>   Input::_axisActions {};
+
+	bool Input::GetDown(SDL_KeyCode keyCode) { return _keyDownStates[keyCode]; }
+
+	bool Input::GetPressed(SDL_KeyCode keyCode) { return _keyPressedStates[keyCode] == PressedState::Pressed; }
 
 	void Input::Update(const SDL_Event& event)
 	{
@@ -15,22 +22,10 @@ namespace SplitEngine
 		}
 		if (event.type == SDL_KEYUP)
 		{
+			_keyDownStates[event.key.keysym.sym] = false;
+
 			if (_keyPressedStates[event.key.keysym.sym] == Waiting) { _keyPressedStates[event.key.keysym.sym] = Ready; }
 		}
-	}
-
-	bool Input::GetDown(SDL_KeyCode keyCode)
-	{
-		if (_keyDownStates.find(keyCode) == _keyDownStates.end()) { _keyDownStates[keyCode] = false; }
-
-		return _keyDownStates[keyCode];
-	}
-
-	bool Input::GetPressed(SDL_KeyCode keyCode)
-	{
-		if (_keyPressedStates.find(keyCode) == _keyPressedStates.end()) { _keyPressedStates[keyCode] = Ready; }
-
-		return _keyPressedStates[keyCode] == PressedState::Pressed;
 	}
 
 	void Input::Reset()
@@ -38,6 +33,14 @@ namespace SplitEngine
 		for (const auto& pair : _keyPressedStates)
 		{
 			if (pair.second == PressedState::Pressed) { _keyPressedStates[pair.first] = PressedState::Waiting; }
+		}
+
+		for (auto& [key, buttonAction] : _buttonActions) { buttonAction.Cached = false; }
+
+		for (auto& [key, axisAction] : _axisActions)
+		{
+			axisAction.PressedCached = false;
+			axisAction.DownCached    = false;
 		}
 	}
 }
