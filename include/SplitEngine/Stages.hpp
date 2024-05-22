583235cd8f1827c1a43d58014b6ffecb0ccdc035
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-05-23 01:25:50 +0200

    Put Engine Stage enums into their own namespace to avoid polluting the SplitEngine namespace

--- include/SplitEngine/Stages.hpp
+++ include/SplitEngine/Stages.hpp
@@ -3,30 +3,36 @@
 
 namespace SplitEngine
 {
-	enum EngineStage : uint8_t
+	namespace EngineStage
 	{
-		/**
-		 * The Begin Frame Stage executes stuff like delta time calculation and input polling
-		 */
-		BeginFrame = 20,
+		enum EngineStage : uint8_t
+		{
+			/**
+			 * The Begin Frame Stage executes stuff like delta time calculation and input polling
+			 */
+			BeginFrame = 20,
 
-		/**
-		 * This stage is used to start rendering (renderpass getting binded etc...)
-		 */
-		BeginRendering = 150,
+			/**
+			 * This stage is used to start rendering (renderpass getting binded etc...)
+			 */
+			BeginRendering = 150,
 
-		/**
-		 * This stage ends the rendering process (renderpass getting unbinded and swapchain image retrieval etc...)
-		 */
-		EndRendering = 200,
-	};
+			/**
+			 * This stage ends the rendering process (renderpass getting unbinded and swapchain image retrieval etc...)
+			 */
+			EndRendering = 200,
+		};
+	}
 
-	enum EngineStageOrder
+	namespace EngineStageOrder
 	{
-		BeginFrame_StatisticsSystem    = -11'000,
-		BeginFrame_TimeSystem          = -10'000,
-		BeginFrame_SDLEventSystem      = -10'000,
-		BeginRendering_RenderingSystem = -10'000,
-		EndRendering_RenderingSystem   = -10'000,
-	};
+		enum EngineStageOrder
+		{
+			BeginFrame_StatisticsSystem    = -11'000,
+			BeginFrame_TimeSystem          = -10'000,
+			BeginFrame_SDLEventSystem      = -10'000,
+			BeginRendering_RenderingSystem = -10'000,
+			EndRendering_RenderingSystem   = -10'000,
+		};
+	}
 }
