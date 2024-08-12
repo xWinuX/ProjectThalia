a930073c970fc0461ba2b71f36017a8778ccebfc
WinuX <37900754+xWinuX@users.noreply.github.com>
2024-08-12 15:51:43 +0200

    fixed uint type bug

--- include/SplitEngine/ECS/Entity.hpp
+++ include/SplitEngine/ECS/Entity.hpp
@@ -6,15 +6,15 @@ namespace SplitEngine::ECS
 	struct Entity
 	{
 		public:
-			uint64_t archetypeIndex     = -1;
-			uint64_t componentIndex     = -1;
-			uint64_t moveArchetypeIndex = -1;
-			uint64_t moveComponentIndex = -1;
-			uint8_t  group              = -1;
-			uint64_t groupIndex         = -1;
+			uint64_t archetypeIndex     = -1ull;
+			uint64_t componentIndex     = -1ull;
+			uint64_t moveArchetypeIndex = -1ull;
+			uint64_t moveComponentIndex = -1ull;
+			uint8_t  group              = -1u;
+			uint64_t groupIndex         = -1ull;
 
-			[[nodiscard]] uint64_t GetArchetypeIndex() const { return archetypeIndex != -1 ? archetypeIndex : moveArchetypeIndex; }
+			[[nodiscard]] uint64_t GetArchetypeIndex() const { return archetypeIndex != -1ull ? archetypeIndex : moveArchetypeIndex; }
 
-			[[nodiscard]] uint64_t GetComponentIndex() const { return componentIndex != -1 ? componentIndex : moveComponentIndex; }
+			[[nodiscard]] uint64_t GetComponentIndex() const { return componentIndex != -1ull ? componentIndex : moveComponentIndex; }
 	};
 }
